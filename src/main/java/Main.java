d6c3028d8feea84808fb88152a6345d844667891
Devansh Garg <devanshgarg024@gmail.com>
2026-02-09 21:12:18 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,57 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.concurrent.Executors;
 public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
     //  Uncomment the code below to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
+      try (var executor = Executors.newVirtualThreadPerTaskExecutor();
+           ServerSocket serverSocket = new ServerSocket(port)) {
+
           serverSocket.setReuseAddress(true);
+          System.out.println("Redis server started on port " + port);
           // Wait for connection from client.
          while(true) {
-             clientSocket = serverSocket.accept();
-             InputStream input = clientSocket.getInputStream();
-             byte[] buffer = new byte[1024];
-//
-//             // Read the command from the client
-             while(true){
-                 int bytesRead = input.read(buffer);
-             String message = new String(buffer, 0, bytesRead);
-//             System.out.println(message);
-             if(message.contains("PING")){
-                 clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-             }
-             else if(message.contains("Close")){
-            if (clientSocket != null) {
-              clientSocket.close();
-              break;
-            }
-
-             }
-             }
+             Socket clientSocket = serverSocket.accept();
+             executor.submit(() -> {
+                 handleRequest(clientSocket);
+             });
 
          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         }
   }
+    private static void handleRequest(Socket clientSocket) {
+      try(clientSocket){
+        System.out.println("Processing on: " + Thread.currentThread());
+        InputStream input = clientSocket.getInputStream();
+        OutputStream output = clientSocket.getOutputStream();
+        byte[] buffer = new byte[1024];
+//
+//             // Read the command from the client
+        while(true){
+            int bytesRead = input.read(buffer);
+            String message = new String(buffer, 0, bytesRead);
+//             System.out.println(message);
+            if(message.toLowerCase().contains("ping")){
+                output.write("+PONG\r\n".getBytes());
+            }
+            else if(message.toLowerCase().contains("close")){
+                break;
+            }
+        }
+
+      }
+      catch(IOException e){
+          System.out.println("Error handling Client: "+ e.getMessage());
+        }
+        // Do heavy I/O here (Database, API calls, etc.)
+    }
 }
