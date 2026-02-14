d668eed1855d347020a1f74fef3374411f56d157
Devansh Garg <devanshgarg024@gmail.com>
2026-02-15 01:15:24 +0530

    codecrafters submit [skip ci]

--- src/main/java/OperationCommand.java
+++ src/main/java/OperationCommand.java
@@ -8,6 +8,13 @@ public class OperationCommand {
         if(!Main.db.containsKey(key)){
             Main.db.put(key,"0");
         }
+        try{
+            Integer.parseInt(Main.db.get(key));
+        }
+        catch(NumberFormatException e){
+            response+=("-ERR value is not an integer or out of range\r\n");
+            return response;
+        }
         int value=Integer.parseInt(Main.db.get(key))+1;
         Main.db.put(key,String.valueOf(value));
         response+=(":"+String.valueOf(value)+"\r\n");
