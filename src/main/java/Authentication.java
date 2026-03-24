9447f979d9bb58cab297815dbe3268b579529d22
Devansh Garg <devanshgarg024@gmail.com>
2026-03-24 23:42:42 +0530

    codecrafters submit [skip ci]

--- src/main/java/Authentication.java
+++ src/main/java/Authentication.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 
 public class Authentication {
     public static String acl(ArrayList<String>words){
-//        if(words.get(3).equals("WHOAMI")){
+        if(words.get(3).equals("WHOAMI")){
             return "$7\r\ndefault\r\n";
-//        }
+        }
+        else if(words.get(3).equals("GETUSER")){
+            StringBuilder cmdBuilder=new StringBuilder();
+            cmdBuilder.append("*2").append("\r\n");
+            cmdBuilder.append("$5\r\nflags\r\n");
+            cmdBuilder.append("*0\r\n");
+            return cmdBuilder.toString();
+        }
+        return "";
     }
 }
